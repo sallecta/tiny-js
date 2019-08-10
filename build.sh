d17e3f786fe425280346bf2b7c777c79076574ab
sallecta <sallecta@inbox.ru>
2019-08-10 19:29:40 +0300

    source files moved to separate dir

--- build.sh
+++ build.sh
@@ -13,15 +13,17 @@ objects=""
 func_make_obj () {
 	step=$(($step + 1))
 	steptarget=$1
+	if [ "$2" == "" ]; then targetdir=""; else targetdir="$2/"; fi
 	objects=$objects" $build/$steptarget.o"
 	echo "$step. make_obj: $steptarget"
-	$compile $steptarget.cpp -o $build/$steptarget.o
+	$compile $targetdir$steptarget.cpp -o $build/$steptarget.o
 }
 func_make_exe () {
 	step=$(($step + 1))
 	steptarget=$1
+	if [ "$2" == "" ]; then targetdir=""; else targetdir="$2/"; fi
 	echo "$step. make_exe: $steptarget"
-	$copileAndLink $steptarget.cpp $objects -o $build/$steptarget
+	$copileAndLink $targetdir$steptarget.cpp $objects -o $build/$steptarget
 }
 func_final () {
 	step=$(($step + 1))
@@ -31,15 +33,15 @@ func_final () {
 	echo "Build complete."
 }
 
-func_make_obj "tinyjs"
+func_make_obj "tinyjs" "src"
 
-func_make_obj "tinyjs-functions"
+func_make_obj "tinyjs-functions" "src"
 
-func_make_obj "tinyjs-functions-math"
+func_make_obj "tinyjs-functions-math" "src"
 
-func_make_exe "run_tests"
+func_make_exe "run_tests" "src"
 
-func_make_exe "script"
+func_make_exe "script" "src"
 
 func_final
 
